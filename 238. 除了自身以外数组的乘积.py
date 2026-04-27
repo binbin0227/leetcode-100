b1f9fc8988e0373df1d250f6c56d145e34775a7d
binbin0227 <1210850015@qq.com>
2026-04-27 23:58:09 +0800

    feat:普通数组

--- 238. 除了自身以外数组的乘积.py
+++ 238. 除了自身以外数组的乘积.py
@@ -5,14 +5,40 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        left_list = [nums[0]]
+        L = [1] * n
+        result = [1] * n
+        
+        # 1. 填充 L 数组 (L[i] 是 nums[i] 左侧所有元素的乘积，不包括L[i])
+        # 对于 i = 0，左侧没有元素，L[0] 就是默认的 1
         for i in range(1, n):
-            left_list.append(left_list[i - 1] * nums[i])
-        right_list = [nums[-1]]
-        for i in range(n - 1, 0,-1):
-            right_list.insert(0,right_list[i + 1] * nums[i])
-        result = []
-        result.append(right_list[1])
-        for i in range(1, n - 1):
-            result.append(left_list[i - 1] * right_list[i + 1])
+            L[i] = L[i - 1] * nums[i - 1]
+        right_product = 1
+        
+        # 2. right_product表示右侧的数的乘积，也就是R[i]
+        for i in range(n - 1, -1, -1):
+            result[i]=L[i]*right_product
+            right_product *=nums[i]
+        return result
+    
+def productExceptSelf2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # 1. 初始化预分配数组，默认全为 1，完美避开 insert 导致的性能问题
+        n = len(nums)
+        L = [1] * n
+        R = [1] * n
+        result = [1] * n
+        
+        # 2. 填充 L R 数组 (L[i] 是 nums[i] 左侧所有元素的乘积，不包括L[i])
+        # 对于 i = 0，左侧没有元素，L[0] 就是默认的 1
+        for i in range(1, n):
+            L[i] = L[i - 1] * nums[i - 1]
+        for i in range(n - 2, -1, -1):
+            R[i] = R[i + 1] * nums[i + 1]
+            
+        # 3. 合并结果
+        for i in range(n):
+            result[i] = L[i] * R[i]
         return result
---
+++ 41. 缺失的第一个正数.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            # 只有数字1-n有位置坐；如果它在正确位置（数字3在索引2）就不用换位
+            while 0 < nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+        # 换完座位后，再巡视一圈，如果谁没坐在该坐的位置上，那本该坐在这里的数字就是缺失的第一个正数
+        for i in range(0, n):
+            if nums[i] != i + 1:
+                return i + 1
+        # 如果大家极其完美地排排坐好了 (比如 [1, 2, 3])，那就缺下一个4
+        return n + 1
