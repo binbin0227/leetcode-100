879740c0952504254864c2edbc0c84fc1cd8a4ec
binbin0227 <1210850015@qq.com>
2026-05-01 19:40:14 +0800

    feat:矩阵

---
+++ 160. 相交链表.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        # pa 走完 链表A 后去走 链表B ，pb 走完 链表B 后去走 链表A
+        # 有相交节点 -> return 循环结束时的 pa
+        # 无相交节点 -> return None
+        pa, pb = headA, headB
+        while pa != pb:
+            if pa is None:
+                pa = headB
+            else:
+                pa = pa.next
+            if pb is None:
+                pb = headA
+            else:
+                pb = pb.next
+        return pa
+
+    # 超时解法
+    def getIntersectionNode2(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        pa = headA
+        while pa is not None:
+            pb = headB
+            while pb is not None:
+                if pa == pb:
+                    return pa
+                pb = pb.next
+            pa = pa.next
---
+++ 206. 反转链表.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    # 三指针法
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        curr = head
+        pre = None
+        while curr:
+            n = curr.next
+            curr.next = pre
+            pre = curr
+            curr = n
+        return pre
