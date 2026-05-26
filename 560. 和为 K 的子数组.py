0c043971d1230463ae1086294fc5d19d5a3730ef
binbin0227 <1210850015@qq.com>
2026-05-26 20:29:13 +0800

    feat:子串

--- 560. 和为 K 的子数组.py
+++ 560. 和为 K 的子数组.py
@@ -8,7 +8,7 @@ class Solution(object):
         result = 0
         pre_sum = 0
         n = len(nums)
-        pre_sum_dict = {0: 1}
+        pre_sum_dict = {0: 1} #表示 pre_sum 为 0 的个数为 1
         #为什么要加0:1
         # 假设 nums = [3, 4, 7], k = 3。如果没有 {0: 1}：
         # 遍历到 3：pre_sum = 3。寻找 target = 3 - 3 = 0。
